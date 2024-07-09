d679a8e3c7e7da06c59b73f1285578d5af34b38a
Abhilash Kumar Giri <114879092+Abhi-lash19@users.noreply.github.com>
2024-07-09 10:46:24 +0000

    changed code for AWS secrets

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,20 @@
 variable "region" {
-    default = "ap-south-1"
+  description = "The AWS region to deploy in"
+  type        = string
 }
-variable "public_key" {
-  
-}
-variable "private_key" {
-  
-}
+
 variable "key_name" {
-  
+  description = "The name of the key pair"
+  type        = string
+}
+
+variable "public_key" {
+  description = "The public key for SSH access"
+  type        = string
+}
+
+variable "private_key" {
+  description = "The private key for SSH access"
+  type        = string
+  sensitive   = true
 }
