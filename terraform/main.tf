765b4ecf4f15d9021100bfcbf74fdd5e0ebdb1f8
Abhilash Kumar Giri <114879092+Abhi-lash19@users.noreply.github.com>
2024-07-11 05:27:28 +0000

    added ingress rule for ICMP protocol

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,7 @@ resource "aws_instance" "servernode" {
   key_name               = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.maingroup.id]
   iam_instance_profile   = aws_iam_instance_profile.ec2-profile.name
-  subnet_id              = var.subnet_id  # Ensure this is a public subnet
+  subnet_id              = var.subnet_id
 
   connection {
     type        = "ssh"
